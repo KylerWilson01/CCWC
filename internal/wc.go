fc4d4ec0aeba133d61d81e405e148ce645e6a73f
Kyler Wilson <kyler@viticusgroup.org>
2025-02-14 11:59:40 -0800

    refactor

--- internal/wc.go
+++ internal/wc.go
@@ -1,21 +1,37 @@
 package internal
 
 import (
-	"log"
+	"errors"
 	"os"
 )
 
-// WordCount gives the word count of a file with the given file path
-func WordCount(fp string) (int, error) {
+const (
+	// CouldNotOpenFileError is the error that happens when we fail to open a file
+	CouldNotOpenFileError = "Could not open the given the file"
+	// CouldNotGetStatsError is the error that happens when we fail to retrieve stats of a file
+	CouldNotGetStatsError = "Could not get the stats of the given the file"
+)
+
+// Record holds information about a file
+type Record struct {
+	ByteSize int
+}
+
+// AnalyzeFile gives the word count of a file with the given file path
+func AnalyzeFile(fp string) (*Record, error) {
 	f, err := os.Open(fp)
 	if err != nil {
-		log.Fatalf("Could not open the given file of %s", fp)
+		return nil, errors.New(CouldNotOpenFileError)
 	}
 
 	fi, err := f.Stat()
 	if err != nil {
-		log.Fatalf("Could not open the given file of %s", fp)
+		return nil, errors.New(CouldNotGetStatsError)
 	}
 
-	return int(fi.Size()), nil
+	r := Record{
+		ByteSize: int(fi.Size()),
+	}
+
+	return &r, nil
 }
--- internal/wc_test.go
+++ internal/wc_test.go
@@ -6,13 +6,22 @@ import (
 	"github.com/KylerWilson01/CCWC/internal"
 )
 
-func TestWordCount(t *testing.T) {
-	length, err := internal.WordCount("testdata/test.txt")
-	if err != nil {
-		t.Fatal()
+func TestByteSize(t *testing.T) {
+	tests := []struct {
+		name, filePath string
+		want           int
+	}{
+		{"Correct byte size", "testdata/test.txt", 342190},
 	}
 
-	if length != 342190 {
-		t.Fail()
+	for _, test := range tests {
+		record, err := internal.AnalyzeFile(test.filePath)
+		if err != nil {
+			t.Errorf("\nCould not open %s\n", test.filePath)
+		}
+
+		if record.ByteSize != test.want {
+			t.Errorf("\nWant: %d\nGot: %d", test.want, record.ByteSize)
+		}
 	}
 }
